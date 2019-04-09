c8f05a97bd0c479c9f99455e8451a9dd56212f71
Vladislav Fitc <vladislav.fitc@algolia.com>
2019-04-09 18:02:42 +0200

    Filter groups as containers tests

--- Tests/Unit Tests/FilterGroupTests.swift
+++ Tests/Unit Tests/FilterGroupTests.swift
@@ -11,24 +11,48 @@ import XCTest
 @testable import InstantSearchClient
 
 class FilterGroupTests: XCTestCase {
-    
-  func test() {
+  
+  func testAndGroupSingle() {
     
     let group = FilterGroup.And(filters: [
-      Filter.Tag(value: "t"),
+      Filter.Tag(value: "tag"),
+    ])
+    
+    XCTAssertEqual(group.sqlForm, "\"_tags\":\"tag\"")
+    
+  }
+  
+  func testAndGroupMultiple() {
+    
+    let group = FilterGroup.And(filters: [
+      Filter.Tag(value: "tag"),
       Filter.Numeric(attribute: "size", operator: .equals, value: 40),
       Filter.Facet(attribute: "brand", stringValue: "sony")
     ])
+
+    XCTAssertEqual(group.sqlForm, "( \"_tags\":\"tag\" AND \"size\" = 40.0 AND \"brand\":\"sony\" )")
     
-    let orGroup = FilterGroup.Or(filters: [
+  }
+  
+  func testOrGroupSingle() {
+    
+    let group = FilterGroup.Or(filters: [
+      Filter.Facet(attribute: "brand", stringValue: "philips")
+    ])
+    
+    XCTAssertEqual(group.sqlForm, "\"brand\":\"philips\"")
+
+  }
+  
+  func testOrGroupMultiple() {
+    
+    let group = FilterGroup.Or(filters: [
       Filter.Facet(attribute: "brand", stringValue: "philips"),
       Filter.Facet(attribute: "diagonal", floatValue: 42),
       Filter.Facet(attribute: "featured", boolValue: true),
     ])
-    
-    let groups: [FilterGroupType & SQLSyntaxConvertible] = [group, orGroup]
-    
-    print(groups.sqlForm)
+
+    XCTAssertEqual(group.sqlForm, "( \"brand\":\"philips\" OR \"diagonal\":\"42.0\" OR \"featured\":\"true\" )")
     
   }
   
